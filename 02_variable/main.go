aa6b62715468f377490379e9633f16c71b9341d0
tuocangyu <1054356997@qq.com>
2022-06-04 13:15:59 +0800

    添加 变量章节序号

--- 02_variable/main.go
+++ 02_variable/main.go
@@ -4,16 +4,18 @@ package main
 
 import "fmt"
 
+// 一.全局变量
 // 这是全局变量 类型可不写 根据值自动推断变量类型（类型推导）
 var testOne string = "哈哈哈"
 
+// 二.默认值
 // 如果变量没有赋值
 // int float类型默认值为0
 // string类型默认值为“”
 // bool类型默认为false
 var num int
 
-// 多个变量声明方式
+// 三.多个变量声明方式
 // 类型相同
 // var num1, num2 int
 // 类型推导
@@ -23,10 +25,11 @@ var (
 	str  = "better-yourself"
 )
 
-// 这是一个全局常量
+// 四.全局常量
 const testTwo = "嘿嘿"
 
 func knowledge() {
+	// 五.局部变量
 	// 局部变量可以使用 name := “哈哈哈” 再次赋值直接使用name="嘿嘿嘿"就可
 	name := "哈哈哈"
 	// name = 123 这是错误的 不可更改数据类型
